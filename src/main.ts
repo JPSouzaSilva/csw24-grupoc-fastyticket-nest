3a653c598587afb28717932901356d793c040e9b
lfcr9311 <lfcr93@gmail.com>
2024-10-01 22:16:16 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder().setTitle('Fasty Ticket API').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+  app.enableShutdownHooks()
   await app.listen(3000)
 }
 bootstrap()
