3aa942879bfa5397455e060d585e6d06a26c699f
João Silva <jpsouzadasilva@gmail.com>
2024-10-22 13:56:13 -0300

    wip

--- src/http/controllers/user/user.controller.ts
+++ src/http/controllers/user/user.controller.ts
@@ -14,7 +14,15 @@ import { UserService } from 'src/application/services/user/user.service'
 import { RegisterUserDto } from 'src/http/dtos/user/register.user.dto'
 import { NotificationService } from 'src/application/services/notification/notification.service'
 import { AuthGuard } from 'src/guard/auth.guard'
+import {
+  ApiOperation,
+  ApiBody,
+  ApiResponse,
+  ApiTags,
+  ApiQuery,
+} from '@nestjs/swagger'
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(
@@ -22,36 +30,212 @@ export class UserController {
     private readonly notificationService: NotificationService,
   ) {}
 
+  @ApiOperation({
+    summary: 'User Login',
+    description: 'Performs user login in the application.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Login successful.',
+    type: LoginDto,
+    example: {
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik5vbWUifQ.bt-F3HkbV-vz7IzKJfik_8grERv-YAxsR2j8c-sqNLM',
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Request',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  @ApiBody({
+    description: 'Username, email and tenantId for authentication',
+    required: true,
+    type: LoginDto,
+  })
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.userService.login(loginDto)
   }
 
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'Get User ',
+    description: 'Performs user login in the application.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User Information',
+    example: {
+      _id: 'eaa0fd1c-392b-4491-ac50-accb5967b2fc',
+      props: {
+        email: 'joao@email1.com',
+        name: 'joao',
+        role: 'USER',
+        tenantId: '123',
+        rate: null,
+        balance: 0,
+        verified: 'joao@email1.comjoao123',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid provided data.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
   @Get('profile')
   async getUser(@UserRequest() req) {
     const verified = req.email.concat(req.username).concat(req.tenantId)
     return this.userService.findByVerified(verified)
   }
 
+  @ApiOperation({
+    summary: 'New User Registration',
+    description: 'Creates a new user with the provided data.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User successfully registered.',
+    example: {
+      _id: 'eaa0fd1c-392b-4491-ac50-accb5967b2fc',
+      props: {
+        email: 'joao@email1.com',
+        name: 'joao',
+        role: 'USER',
+        tenantId: '123',
+        rate: null,
+        balance: 0,
+        verified: 'joao@email1.comjoao123',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
+  @ApiBody({
+    description: 'User data for registration',
+    required: true,
+    type: RegisterUserDto,
+  })
   @Post('register')
   async register(@Body() data: RegisterUserDto) {
     return this.userService.register(data)
   }
 
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'New Admin Registration',
+    description: 'Creates a new admin with the provided data.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Admin successfully registered.',
+    example: {
+      _id: 'eaa0fd1c-392b-4491-ac50-accb5967b2fc',
+      props: {
+        email: 'joao@email1.com',
+        name: 'joao',
+        role: 'Admin',
+        tenantId: '123',
+        rate: null,
+        balance: 0,
+        verified: 'joao@email1.comjoao123',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
+  @ApiBody({
+    description: 'User data for registration',
+    required: true,
+    type: RegisterUserDto,
+  })
   @Post('register/admin')
   async registerAdmin(@UserRequest() req, @Body() data: RegisterUserDto) {
     return this.userService.registerAdmin(data, req)
   }
 
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'Get User Balance',
+    description: 'User balance information.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User Balance',
+    example: {
+      balance: 200.0,
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
   @Get('balance')
   async getBalance(@UserRequest() req) {
     return this.userService.getBalance(req.id)
   }
 
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'User Preferences',
+    description: 'Updates preferences of a user.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Updated successfully.',
+    example: {
+      _id: '6d904338-bd26-4dfc-8f48-411584692590',
+      props: {
+        userId: 'eaa0fd1c-392b-4491-ac50-accb5967b2fc',
+        receiveEmail: true,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid provided data.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
+  @ApiQuery({
+    name: 'notify',
+    description: 'Notify preferences',
+    required: true,
+    type: Boolean,
+  })
   @Put('preferences')
   async preferences(
     @UserRequest() req,
--- src/http/dtos/login.user.dto.ts
+++ src/http/dtos/login.user.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class LoginDto {
+  @ApiProperty({
+    description: 'username',
+    example: 'joaosilva123',
+  })
   username: string
+
+  @ApiProperty({
+    description: 'email',
+    example: 'joaosilva@email.com',
+  })
   email: string
+
+  @ApiProperty({
+    description: 'tenantId',
+    example: '123',
+  })
   tenantId: string
 }
