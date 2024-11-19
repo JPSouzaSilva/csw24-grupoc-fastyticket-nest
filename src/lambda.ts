dcd75a7a13509271ecfd8cdaf42e23b69959037a
João Silva <jpsouzadasilva@gmail.com>
2024-11-19 17:48:33 -0300

    trocando pacote

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,18 +1,57 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 import { AppModule } from './app.module'
 
 let cachedServer
 
+async function createServer() {
+  const app = await NestFactory.create(AppModule)
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('FastyTicket')
+    .setDescription('Documentação da API do projeto FastyTicket')
+    .setVersion('1.0')
+    .addSecurity('apiKey', {
+      type: 'http',
+      scheme: 'basic',
+    })
+    .addSecurity('sec0', {
+      type: 'apiKey',
+      in: 'header',
+      name: 'access-token',
+    })
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+
+  app.use(
+    '/docs',
+    apiReference({
+      theme: 'alternate',
+      darkMode: true,
+      layout: 'modern',
+      spec: {
+        content: document,
+      },
+    }),
+  )
+
+  app.enableCors({
+    origin: '*',
+  })
+
+  await app.init()
+
+  return serverlessExpress({
+    app: app.getHttpAdapter().getInstance(),
+  })
+}
+
 export const handler = async (event, context) => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule)
-
-    await nestApp.init()
-
-    cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
-    })
+    cachedServer = await createServer()
   }
 
   return cachedServer(event, context)
